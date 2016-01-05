030ce17deb11b74242ee44df8ba248d3e643b9ef
Minwei Xu <faceswilliam@gmail.com>
2016-01-04 22:43:29 -0800

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+```
+npm install angle-to-direction
+```
+
+# Usage
+```
+var a2d = require('angle-to-direction');
+
+a2d.degree(50); // "North East"
+a2d.degreeAbbr(50); // "NE"
+a2d.radian(0.873); // "North East"
+a2d.radianAbbr(0.873); // "NE"
+```
---
+++ angleToDirection.js
@@ -0,0 +1,117 @@
+
+var angleToDirection = {
+    degree: function (deg) {
+        switch ((deg + 22.5) / 45) {
+            case 1:
+                return "North";
+                break;
+            case 2:
+                return "North East";
+                break;
+            case 3:
+                return "East";
+                break;
+            case 4:
+                return "South East";
+                break;
+            case 5:
+                return "South";
+                break;
+            case 6:
+                return "South West";
+                break;
+            case 7:
+                return "West";
+                break;
+            case 8:
+                return "North West";
+                break;
+        }
+    },
+    degreeAbbr: function (deg) {
+        switch ((deg + 22.5) / 45) {
+            case 1:
+                return "N";
+                break;
+            case 2:
+                return "NE";
+                break;
+            case 3:
+                return "E";
+                break;
+            case 4:
+                return "SE";
+                break;
+            case 5:
+                return "S";
+                break;
+            case 6:
+                return "SW";
+                break;
+            case 7:
+                return "W";
+                break;
+            case 8:
+                return "NW";
+                break;
+        }
+    },
+    radian: function (rad) {
+        switch ((rad + Math.PI / 8) / (Math.PI / 4)) {
+            case 1:
+                return "North";
+                break;
+            case 2:
+                return "North East";
+                break;
+            case 3:
+                return "East";
+                break;
+            case 4:
+                return "South East";
+                break;
+            case 5:
+                return "South";
+                break;
+            case 6:
+                return "South West";
+                break;
+            case 7:
+                return "West";
+                break;
+            case 8:
+                return "North West";
+                break;
+        }
+    },
+    radianAbbr: function (rad) {
+        switch ((rad + Math.PI / 8) / (Math.PI / 4)) {
+            case 1:
+                return "N";
+                break;
+            case 2:
+                return "NE";
+                break;
+            case 3:
+                return "E";
+                break;
+            case 4:
+                return "SE";
+                break;
+            case 5:
+                return "S";
+                break;
+            case 6:
+                return "SW";
+                break;
+            case 7:
+                return "W";
+                break;
+            case 8:
+                return "NW";
+                break;
+        }
+    }
+};
+
+module.exports = angleToDirection;
